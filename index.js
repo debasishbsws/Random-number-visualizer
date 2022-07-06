37ed448117185072139f2134218881319b3cc176
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-07-06 15:08:44 +0530

    adding all the directions

--- index.js
+++ index.js
@@ -36,25 +36,92 @@ function csvToArray(text) {
     return result;
 }
 
-
 function startAnimation() {
-    var canvas = document.getElementById('canvas'), context = canvas.getContext('2d');
-
-    var y = 0;
-    let i = 0;
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    context.fillStyle = "red";
+    let x = 350, y = 350;
+    let i = 100000;
+    xVe = 1;
+    yVe = 1;
     draw();
     function draw() {
-        // console.log("running");
-        for (let x = 0; x < 1450; x++) {
-            if (numbers[i++] > 0.25) {
-                context.fillRect(x, y, 1, 1);
-            }
+        let speed = 5;
+        let n = Math.random();
+        if (n > 0 && n <= 0.1) {
+            x += xVe * speed;
+        } else if (n > 0.1 && n <= 0.2) {
+            x -= xVe * speed;
+        } else if (n > 0.2 && n <= 0.3) {
+            y += yVe * speed;
+        } else if (n > 0.3 && n <= 0.4) {
+            y -= yVe * speed;
+        } else if (n > 0.4 && n <= 0.5) {
+            x += xVe * speed;
+            y += yVe * speed;
+        } else if (n > 0.5 && n <= 0.6) {
+            x -= xVe * speed;
+            y -= yVe * speed;
+        } else if (n > 0.6 && n <= 0.7) {
+            x += xVe * speed;
+            y -= yVe * speed;
+        } else if (n > 0.7 && n <= 0.8) {
+            x -= xVe * speed;
+            y += yVe * speed;
+        } else if (n > 0.8 && n <= 0.9) {
+            context.fillStyle = "blue";
+        } else if (n > 0.9 && n <= 1) {
+            context.fillStyle = "red";
         }
-        y++;
-        if (y < 700) {
-            requestAnimationFrame(draw);
+        // check if the ball is out of the canvas
+        if (x > canvas.width) {
+            x -= xVe * speed;
         }
+        if (y > canvas.height) {
+            y -= yVe * speed;
+        }
+        if (x < 0) {
+            x += xVe * speed;
+        }
+        if (y < 0) {
+            y += yVe * speed;
+        }
+
+        drawPoint(x, y);
+        requestAnimationFrame(draw);
+
     }
+
+    function drawPoint(x, y) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
+        context.arc(x, y, 5, 0, 2 * Math.PI);
+
+        context.fill();
+        // context.stroke();
+        // context.closePath();
+    }
+
 }
 
+// function for drawing the points sequentially
+// function startAnimation() {
+//     var canvas = document.getElementById('canvas'), context = canvas.getContext('2d');
+
+//     var y = 0;
+//     let i = 0;
+//     draw();
+//     function draw() {
+//         // console.log("running");
+//         for (let x = 0; x < 1450; x++) {
+//             if (numbers[i++] > 0.25) {
+//                 context.fillRect(x, y, 1, 1);
+//             }
+//         }
+//         y++;
+//         if (y < 700) {
+//             requestAnimationFrame(draw);
+//         }
+//     }
+// }
 
