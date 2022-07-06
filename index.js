31632d4bfeb10345926a71443a08085b1b885f61
Debasish Biswas <debasishbsws.abc@gmail.com>
2022-07-06 15:22:05 +0530

    Add stop animation

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const button = document.getElementById("button");
 
 let numbers = [];
 
-myForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    console.log("Form submitted");
-    const input = csvFile.files[0];
+// myForm.addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     console.log("Form submitted");
+//     const input = csvFile.files[0];
 
-    const reader = new FileReader();
+//     const reader = new FileReader();
 
-    reader.onload = function (e) {
-        const text = e.target.result;
-        const data = csvToArray(text);
+//     reader.onload = function (e) {
+//         const text = e.target.result;
+//         const data = csvToArray(text);
 
 
-        data.forEach(element => {
-            numbers.push(Number(element[0]));
-        })
-        button.removeAttribute("hidden");
-    };
-    reader.readAsText(input);
-});
+//         data.forEach(element => {
+//             numbers.push(Number(element[0]));
+//         })
+//         button.removeAttribute("hidden");
+//     };
+//     reader.readAsText(input);
+// });
 
 //function csv to array
 function csvToArray(text) {
@@ -35,13 +35,24 @@ function csvToArray(text) {
     }
     return result;
 }
-
+let breakPoint = false;
+function changeButton() {
+    button.innerHTML = "Stop Animation";
+    button.setAttribute('onclick', 'stopAnimation()');
+}
+function stopAnimation() {
+    button.innerHTML = "Start Animation";
+    breakPoint = true;
+    button.setAttribute('onclick', 'startAnimation()');
+}
+let x = 350, y = 350;
 function startAnimation() {
+    changeButton();
+    breakPoint = false;
     const canvas = document.getElementById("canvas");
     const context = canvas.getContext("2d");
     context.fillStyle = "red";
-    let x = 350, y = 350;
-    let i = 100000;
+    // let i = 100000;
     xVe = 1;
     yVe = 1;
     draw();
@@ -49,25 +60,25 @@ function startAnimation() {
         let speed = 5;
         let n = Math.random();
         if (n > 0 && n <= 0.1) {
-            x += xVe * speed;
+            x += speed;
         } else if (n > 0.1 && n <= 0.2) {
-            x -= xVe * speed;
+            x -= speed;
         } else if (n > 0.2 && n <= 0.3) {
-            y += yVe * speed;
+            y += speed;
         } else if (n > 0.3 && n <= 0.4) {
-            y -= yVe * speed;
+            y -= speed;
         } else if (n > 0.4 && n <= 0.5) {
-            x += xVe * speed;
-            y += yVe * speed;
+            x += speed;
+            y += speed;
         } else if (n > 0.5 && n <= 0.6) {
-            x -= xVe * speed;
-            y -= yVe * speed;
+            x -= speed;
+            y -= speed;
         } else if (n > 0.6 && n <= 0.7) {
-            x += xVe * speed;
-            y -= yVe * speed;
+            x += speed;
+            y -= speed;
         } else if (n > 0.7 && n <= 0.8) {
-            x -= xVe * speed;
-            y += yVe * speed;
+            x -= speed;
+            y += speed;
         } else if (n > 0.8 && n <= 0.9) {
             context.fillStyle = "blue";
         } else if (n > 0.9 && n <= 1) {
@@ -75,16 +86,19 @@ function startAnimation() {
         }
         // check if the ball is out of the canvas
         if (x > canvas.width) {
-            x -= xVe * speed;
+            x -= speed;
         }
         if (y > canvas.height) {
-            y -= yVe * speed;
+            y -= speed;
         }
         if (x < 0) {
-            x += xVe * speed;
+            x += speed;
         }
         if (y < 0) {
-            y += yVe * speed;
+            y += speed;
+        }
+        if (breakPoint) {
+            return;
         }
 
         drawPoint(x, y);
@@ -96,10 +110,7 @@ function startAnimation() {
         context.clearRect(0, 0, canvas.width, canvas.height);
         context.beginPath();
         context.arc(x, y, 5, 0, 2 * Math.PI);
-
         context.fill();
-        // context.stroke();
-        // context.closePath();
     }
 
 }
